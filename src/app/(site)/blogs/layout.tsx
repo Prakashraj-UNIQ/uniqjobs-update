01600b9825899c7b7cba4757ea3fb9a9f0b890b3
Prakash Raj Pandiyarajan <prakash.raj@uniqtechnologies.co.in>
2025-12-09 12:48:55 +0530

    commit changes

--- src/app/(site)/blogs/layout.tsx
+++ src/app/(site)/blogs/layout.tsx
@@ -1,10 +1,14 @@
 import BlogAsideWrapper from "@/components/blog/BlogAsideWrapper";
+import { Suspense } from "react";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
     return (
         <div className="min-h-screen  mt-20 md:mt-0">
             <aside className=" overflow-y-auto bg-white z-1000 px-25">
-                <BlogAsideWrapper />
+                <Suspense fallback={<div>Loading filters...</div>}>
+                    <BlogAsideWrapper />
+
+                </Suspense>
             </aside>
             {children}
         </div>);
